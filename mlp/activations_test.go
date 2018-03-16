4ec5bd2fe56748228d92e60c05a624a32e8a45f9
cshenton <charlieshenton@me.com>
2018-03-16 19:47:08 +1100

    testing travis

--- mlp/activations_test.go
+++ mlp/activations_test.go
@@ -1,1 +1,14 @@
 package mlp_test
+
+import (
+	"testing"
+
+	"github.com/cshenton/openai-evolution/mlp"
+)
+
+func TestIdentity(t *testing.T) {
+	x := float64(3.1415926)
+	if x != mlp.Identity(x) {
+		t.Errorf("expected identity to equal %v, but it was %v", x, mlp.Identity(x))
+	}
+}
